b1a73b84e2ff72ae71e21f263ea5aeeb7cbebc50
mrfixit2001 <mrfixit2001@gmail.com>
2023-02-17 12:43:29 -0500

    Cleanup

--- rk3318/make-uboot.sh
+++ rk3318/make-uboot.sh
@@ -1,5 +1,5 @@
 rm idbloader.img uboot.img trust.img
-./mkimage -n rk3328 -T rksd -d "rk3318_ddr_666Mhz_v1.16.bin" idbloader.img
+../mkimage -n rk3328 -T rksd -d "rk3318_ddr_666Mhz_v1.16.bin" idbloader.img
 cat "rk3328_miniloader_v2.50.bin" >> idbloader.img
-./loaderimage --pack --uboot u-boot-dtb.bin uboot.img 0x200000
-./trust_merger --verbose trust.ini
+../loaderimage --pack --uboot u-boot-dtb.bin uboot.img 0x200000
+../trust_merger --verbose trust.ini
