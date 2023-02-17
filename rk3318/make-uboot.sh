22b866d0bc33ad09b6c0bb3d951409d1919240ae
mrfixit2001 <mrfixit2001@gmail.com>
2023-02-17 12:40:36 -0500

    Move some things around and add rk3318

--- rk3318/make-uboot.sh
+++ rk3318/make-uboot.sh
@@ -1,4 +1,5 @@
-./mkimage -n rk3328 -T rksd -d "rk3328_ddr_786MHz_v1.13.bin" idbloader.img
+rm idbloader.img uboot.img trust.img
+./mkimage -n rk3328 -T rksd -d "rk3318_ddr_666Mhz_v1.16.bin" idbloader.img
 cat "rk3328_miniloader_v2.50.bin" >> idbloader.img
 ./loaderimage --pack --uboot u-boot-dtb.bin uboot.img 0x200000
 ./trust_merger --verbose trust.ini
